b749d10193c410c2521119033ccb930e9fb184f2
Seiichi Yonezawa <nzwsch@gmail.com>
2021-09-04 13:26:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-const canvas = document.getElementById('freshmint')
-const roll = document.getElementById('roll')
+const canvas = document.getElementById('freshmint');
+const roll = document.getElementById('roll');
 
-const resetCanvas = (ctx) => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
+const colors = [
+  '#92b372', // mint
+  '#6cabcd', // aqua
+  '#5b73c4', // blue
+  '#aa876a', // brown
+  '#9d9d9d', // grey
+  '#db9d61', // orange
+  '#c76199', // pink
+  '#8c6ec9', // purple
+  '#c15b58', // red
+  '#c8ac69', // sand
+  '#5aaa9a', // teal
+  '#e7bc0d', // yellow
+];
 
-const centerPosition = () => [canvas.width / 2, canvas.height / 2]
+const resetCanvas = (ctx, w, h) => {
+  ctx.clearRect(0, 0, w, h);
+};
+
+const drawBaum = (ctx, w, h, r, i) => {
+  const x = w / 2,
+    y = h / 2,
+    j = i + 1,
+    p = Math.PI,
+    k = 2 / colors.length,
+    o = 0.5 + 1 / colors.length;
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.arc(x, y, r, p * (i * k - o), p * (j * k - o), false);
+  ctx.lineTo(x, y);
+  ctx.fillStyle = colors[i];
+  ctx.fill();
+};
 
 const drawRoulette = () => {
-  const ctx = canvas.getContext('2d')
+  const context = canvas.getContext('2d'),
+    width = canvas.width,
+    height = canvas.height,
+    radius = Math.min(canvas.width, canvas.height) / 2;
 
-  resetCanvas(ctx)
+  resetCanvas(context, width, height);
 
-  console.log(centerPosition())
-}
+  for (let i = 0; i < colors.length; i++) {
+    drawBaum(context, width, height, radius, i);
+  }
+};
 
-drawRoulette()
+drawRoulette();
 
-roll.addEventListener('click', () => drawRoulette())
+roll.addEventListener('click', () => drawRoulette());
